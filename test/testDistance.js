cd50b7efb030d5da2431e6d1acc39bfc1b2ba085
Peter Walker <peterw@suprafax.net>
2018-07-19 13:33:17 -0700

    Eliminate compilers deprecation warning

--- test/testDistance.js
+++ test/testDistance.js
@@ -6,8 +6,8 @@ var assert = require('chai').assert
 describe('formatDistance', function () {
   describe('#formatDistance()', function () {
     it('works in a basic sense', function () {
-      const formatted = formatDistance(100, {metricUnits: true})
-      assert.equal(formatted.value, 100)
+      assert.equal(formatDistance(100, {metricUnits: true}).value, 100)
+      assert.equal(formatDistance(10000, {metricUnits: true}).value, 10)
     })
     it('accepts bigUnits param', function () {
       const formatted = formatDistance(100, {metricUnits: true, bigUnits: true})
