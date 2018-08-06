874e137d3d23070a8cb8d75e313c95b3a7cb9279
Quinn Rohlf <qr@qrohlf.com>
2018-08-06 09:18:29 -0700

    verify correct units as well as correct scalar

--- src/formatDistance.test.js
+++ src/formatDistance.test.js
@@ -5,16 +5,16 @@ import {expect} from 'chai'
 describe('formatDistance', function () {
   describe('#formatDistance()', function () {
     it('works in a basic sense', function () {
-      expect(formatDistance(100, {metricUnits: true}).value).to.equal(100)
-      expect(formatDistance(10000, {metricUnits: true}).value).to.equal(10)
+      expect(formatDistance(100, {metricUnits: true})).to.include({value: 100, long: 'meters'})
+      expect(formatDistance(10000, {metricUnits: true})).to.include({value: 10, long: 'kilometers'})
     })
     it('accepts bigUnits param', function () {
       const formatted = formatDistance(100, {metricUnits: true, bigUnits: true})
-      expect(formatted.value).to.equal(0.1)
+      expect(formatted).to.include({value: 0.1, long: 'kilometers'})
     })
     it('accepts smallUnits param', function () {
       const formatted = formatDistance(15000, {metricUnits: true, smallUnits: true})
-      expect(formatted.value).to.equal(15000)
+      expect(formatted).to.include({value: 15000, long: 'meters'})
     })
   })
 })
