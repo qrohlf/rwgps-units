f610029488879ce0af33f83716164aa14646d66e
Quinn Rohlf <qr@qrohlf.com>
2018-08-06 09:12:34 -0700

    use BDD format and rwgps naming convention

--- src/formatDistance.test.js
+++ src/formatDistance.test.js
@@ -1,21 +1,20 @@
 /* eslint-env mocha */
 import {formatDistance} from '../src/formatDistance'
-
-var assert = require('chai').assert
+import {expect} from 'chai'
 
 describe('formatDistance', function () {
   describe('#formatDistance()', function () {
     it('works in a basic sense', function () {
-      assert.equal(formatDistance(100, {metricUnits: true}).value, 100)
-      assert.equal(formatDistance(10000, {metricUnits: true}).value, 10)
+      expect(formatDistance(100, {metricUnits: true}).value).to.equal(100)
+      expect(formatDistance(10000, {metricUnits: true}).value).to.equal(10)
     })
     it('accepts bigUnits param', function () {
       const formatted = formatDistance(100, {metricUnits: true, bigUnits: true})
-      assert.equal(formatted.value, 0.1)
+      expect(formatted.value).to.equal(0.1)
     })
     it('accepts smallUnits param', function () {
       const formatted = formatDistance(15000, {metricUnits: true, smallUnits: true})
-      assert.equal(formatted.value, 15000)
+      expect(formatted.value).to.equal(15000)
     })
   })
 })
