d9f9c1ba90a6d594eca2ec4c1eac66ce587b727d
Quinn Rohlf <qr@qrohlf.com>
2019-09-11 14:17:50 -0600

    fix comment

--- src/formatPace.js
+++ src/formatPace.js
@@ -1,7 +1,6 @@
 import {convert} from './conversions'
 import * as unitTypes from './unitTypes'
 
-// value is expected to be in native rwgps grade units (%)
 export const formatPace = (secondsPerKm, {metricUnits} = {}) => {
   if (metricUnits) {
     // unsupported as of right now, but I could see 'degree' being a possiblilty
